8c65aabed80156d27da70ad8404330fc1cab0477
nekonekoni7 <45564812+nekonekoni7@users.noreply.github.com>
2020-05-07 23:41:17 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,15 @@
 
 #!/usr/bin/env bash
-bash 4-3.sh -h | tee -a 4-2.txt
-bash 4-3.sh -a | tee -a 4-2.txt
-bash 4-3.sh -b | tee -a 4-2.txt
-bash 4-3.sh -c | tee -a 4-2.txt
-bash 4-3.sh -d | tee -a 4-2.txt
-bash 4-3.sh -u | tee -a 4-2.txt
+bash 4-2.sh -h | tee -a text.txt
+bash 4-2.sh -a | tee -a text.txt
+bash 4-2.sh -b | tee -a text.txt
+bash 4-2.sh -c | tee -a text.txt
+bash 4-2.sh -d | tee -a text.txt
+
+
+bash 4-3.sh -h | tee -a text.txt
+bash 4-3.sh -a | tee -a text.txt
+bash 4-3.sh -b | tee -a text.txt
+bash 4-3.sh -c | tee -a text.txt
+bash 4-3.sh -d | tee -a text.txt
+bash 4-3.sh -u /images/NASA-logosmall.gif | tee -a text.txt
