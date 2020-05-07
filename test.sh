2f01da485100334cb43509d4cc18b78ca49abe1c
nekonekoni7 <45564812+nekonekoni7@users.noreply.github.com>
2020-05-07 16:09:32 +0800

    Delete test.sh

--- test.sh
+++
@@ -1,8 +0,0 @@
-
-#!/usr/bin/env bash
-bash 4-3.sh -h | tee -a 4-2.txt
-bash 4-3.sh -a | tee -a 4-2.txt
-bash 4-3.sh -b | tee -a 4-2.txt
-bash 4-3.sh -c | tee -a 4-2.txt
-bash 4-3.sh -d | tee -a 4-2.txt
-bash 4-3.sh -u | tee -a 4-2.txt
